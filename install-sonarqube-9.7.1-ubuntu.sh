81a4a00cc482b0b306dac6f1f4520826e1c52f6c
Varun HM <110014285+varunn777@users.noreply.github.com>
2024-12-13 20:10:59 +0530

    Update install-sonarqube-9.7.1-ubuntu.sh

--- install-sonarqube-9.7.1-ubuntu.sh
+++ install-sonarqube-9.7.1-ubuntu.sh
@@ -10,16 +10,17 @@ echo "################################################################"
 # Installing necessary packages
 echo "\n\n*****Installing necessary packages"
 sudo apt-get update -y > /dev/null 2>&1
-sudo apt-get install -y default-jre unzip > /dev/null 2>&1
+sudo apt-get install -y openjdk-17-jdk unzip > /dev/null 2>&1
 echo "            -> Done"
 
-# Downloading SonarQube 9.7.1 version to OPT folder
-echo "*****Downloading SonarQube 9.7.1 version"
+sudo apt-get update -y > /dev/null 2>&1
+# Downloading SonarQube 10.6.0.92116 version to OPT folder
+echo "*****Downloading SonarQube 10.6.0.92116 version"
 cd /opt 
 sudo rm -rf sonarqube*
-sudo wget -q https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.7.1.62043.zip
-sudo unzip -q sonarqube-9.7.1.62043.zip -d /opt/sonarqube 1>/dev/null
-sudo rm -rf sonarqube-9.7.1.62043.zip
+sudo wget -q https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.6.0.92116.zip
+sudo unzip -q sonarqube-10.6.0.92116.zip -d /opt/sonarqube 1>/dev/null
+sudo rm -rf sonarqube-10.6.0.92116.zip
 echo "            -> Done"
 
 # Changing Ownership as Sonarqube Does not work with Root User
@@ -30,8 +31,9 @@ echo "            -> Done"
 # Starting SonarQube Service
 echo "*****Starting SonarQube Server"
 cd /opt
-sudo su -m ubuntu -c "./sonarqube/sonarqube-9.7.1.62043/bin/linux-x86-64/sonar.sh start 1>/dev/null"
+sudo su -m ubuntu -c "./sonarqube/sonarqube-10.6.0.92116/bin/linux-x86-64/sonar.sh start 1>/dev/null"
 
+sudo apt-get update -y > /dev/null 2>&1
 
 # Check if SonarQube is working
 echo "\n################################################################ \n"
